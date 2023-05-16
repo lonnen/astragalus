0c0735818c612b39d58d0a339d6fc883fbada5e9
lonnen <chris.lonnen@gmail.com>
2023-05-16 01:20:15 -0700

    fixup

--- astragalus/game.py
+++ astragalus/game.py
@@ -18,8 +18,8 @@ class KnucklebonesGame(object):
         self.moves.pop()
 
     def score(self):
-        '''Positive score means player 1 is ahead,
-        negative score means player 2'''
+        """Positive score means player 1 is ahead,
+        negative score means player 2"""
         p1, p2 = self.game.scores()
         return p1 - p2
 
@@ -33,24 +33,24 @@ class KnucklebonesGame(object):
     def copy(self):
         return self.load(self.state())
 
+
 class KnucklebonesBoard(object):
     def __init__(self):
-        self.raw_board = [0] * 18 # 2x3x3
+        self.raw_board = [0] * 18  # 2x3x3
         self.moves = []
 
     def rows(self):
-        return list(zip(*[iter(self.raw_board)]*3))
+        return list(zip(*[iter(self.raw_board)] * 3))
 
     def boards(self):
-        return list(zip(*[iter(self.rows())]*3))
+        return list(zip(*[iter(self.rows())] * 3))
 
     def generate_legal_moves(self):
         """Check to see if either player board is full, indicating the game is over"""
-        #rows = [self.board[i : i + 3] for i in range(0, len(self.board), 3)]
-        #empty_spaces = [index for (index, cell) in enumerate(self.board) if cell == 0]
+        # rows = [self.board[i : i + 3] for i in range(0, len(self.board), 3)]
+        # empty_spaces = [index for (index, cell) in enumerate(self.board) if cell == 0]
         boards = [chain.from_iterable(player_board) for player_board in self.board]
         return any(all(cell is not 0 for cell in board) for board in boards)
-
 
     def is_game_over():
         pass
@@ -59,7 +59,5 @@ class KnucklebonesBoard(object):
         pass
 
     def pop():
-        """
-        Restores the previous board position
-        """
+        """Restores the previous board position"""
         pass
