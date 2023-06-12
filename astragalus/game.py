4ae451d8789df9de2242bc4b10930561f74cf987
lonnen <chris.lonnen@gmail.com>
2023-06-12 02:14:48 -0700

    add relative_position_to_raw_board_position and tests
    this is a longwinded but convenient function to have

--- astragalus/game.py
+++ astragalus/game.py
@@ -41,20 +41,25 @@ class KnucklebonesBoard(object):
     def __init__(self):
         self.raw_board = board = [0] * 18  # 2x3x3
         self.boards = (board[: (len(board) // 2)], board[(len(board) // 2) :])
-        self.rows = (self.board[i : i + 3] for i in range(0, len(self.board), 3))
+        self.columns = (self.board[i : i + 3] for i in range(0, len(self.board), 3))
         self.moves = []
         self.turn = PROTAGONIST
 
+    @staticmethod
+    def relative_positon_to_raw_board_position(board, column, row):
+        return (board * 3 * 3) + (column * 3) + row
+
     def generate_legal_moves(self) -> List[int]:
         """Return the index of any row with space for another number"""
-        return [index for (index, row) in enumerate(self.rows) if 0 in row]
+        return [index for (index, col) in enumerate(self.columns) if 0 in col]
 
     def is_game_over(self) -> bool:
         """the game is over when either board is full"""
         return any(all(board) for board in self.boards)
 
-    def push(self, column) -> None:
-        # calculate the move
+    def push(self, column, dice_roll) -> None:
+        board = self.boards[(0 if self.turn is PROTAGONIST else 1)]
+        other_board = self.boards[(0 if self.turn is ANTAGONIST else 1)]
 
         self.turn = not self.turn
         self.moves.push(column)
--- tests/test_astragalus.py
+++ tests/test_astragalus.py
@@ -1,7 +1,19 @@
 import unittest
 
-import astragalus
+from astragalus import KnucklebonesBoard
 
 class TestBoard(unittest.TestCase):
+
+    def test_relative_positon_to_raw_board_position(self):
+        rp2rbp = KnucklebonesBoard.relative_positon_to_raw_board_position
+
+        positions = []
+        for board in (0,1):
+            for col in (0, 1, 2):
+                for row in (0, 1, 2):
+                    positions.append(rp2rbp(board, col, row))
+
+        self.assertEqual(positions, list(range(2 * 3 * 3)))
+
     def test_board(self):
         assert True
