78281705cc55acb65ddfd508df09b3b8db75feba
lonnen <chris.lonnen@gmail.com>
2023-07-14 00:44:24 -0700

    test pop
    this tests the reversible pop method by evaluating a whole can, and then undoing it one play at a time verifying that every step of the undo matches the outcomes of the initial game

--- tests/test_astragalus.py
+++ tests/test_astragalus.py
@@ -1,6 +1,6 @@
 import unittest
 
-from astragalus import KnucklebonesBoard
+from astragalus import KnucklebonesBoard, PROTAGONIST, ANTAGONIST
 
 class TestBoard(unittest.TestCase):
 
@@ -71,7 +71,63 @@ class TestBoard(unittest.TestCase):
             board.push(column, roll)
             self.assertEqual(board.scores(), [antagonist_total, protagonist_total])
         self.assertTrue(board.is_game_over())
-        # assert Antagonist victory
+        self.assertTrue(board.turn == ANTAGONIST)
 
-        def test_pop(self):
-            pass
+    def test_pop(self):
+        state_log = [
+            # roll, column, antagonist total, protagonist total
+            (1, 1, 1, 0),
+            (1, 1, 0, 1),
+            (6, 2, 6, 1),
+            (2, 3, 6, 3),
+            (4, 1, 10, 3),
+            (5, 2, 10, 8),
+            (5, 2, 15, 3),
+            (1, 1, 15, 6),
+            (3, 2, 18, 6),
+            (4, 1, 14, 10),
+            (1, 1, 15, 6),
+            (5, 2, 10, 11),
+            (3, 2, 19, 11),
+            (5, 2, 19, 26),
+            (2, 3, 21, 24),
+            (6, 2, 15, 30),
+            (5, 2, 20, 10),
+            (4, 1, 20, 22),
+            (1, 1, 23, 22),
+            (3, 1, 23, 25),
+            (4, 1, 27, 9),
+            (5, 2, 22, 14),
+            (6, 2, 28, 8),
+            (6, 2, 22, 14),
+            (4, 2, 26, 14),
+            (3, 2, 14, 17),
+            (5, 2, 19, 12),
+            (2, 3, 17, 14),
+            (3, 2, 20, 11),
+            (4, 1, 16, 15),
+            (5, 3, 21, 15),
+            (1, 2, 21, 16),
+            (4, 1, 25, 12),
+            (2, 1, 25, 14),
+            (6, 3, 31, 14),
+            (6, 3, 25, 20),
+            (6, 3, 31, 14),
+            (5, 3, 26, 19),
+            (4, 3, 30, 19),
+            (3, 2, 27, 22),
+            (6, 3, 45, 22),
+            (2, 1, 45, 28),
+            (1, 2, 46, 27),
+        ]
+        board = KnucklebonesBoard()
+        for roll, column, antagonist_total, protagonist_total in state_log:
+            board.push(column, roll)
+        # now again, in reverse!
+        state_log.reverse()
+        for roll, column, antagonist_total, protagonist_total in state_log[1:]:
+            board.pop()
+            self.assertEqual(board.scores(), [antagonist_total, protagonist_total])
+
+        self.assertEqual(len(board.moves), 1)
+        self.assertEqual(board.turn, ANTAGONIST)
