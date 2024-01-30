d46af433c19a495e38fa99de871eeda02d7bdeca
lonnen <chris.lonnen@gmail.com>
2024-01-29 23:55:55 -0800

    add the basic API for actually playing the game

--- astragalus/game.py
+++ astragalus/game.py
@@ -3,3 +3,33 @@ A library for the minigame Knucklebones from Cult of the Lamb
 """
 
 from .board import *  # noqa: F403,F401
+
+
+class KnucklebonesGame(object):
+    def __init__(self):
+        pass
+
+    def valid_moves(self):
+        raise NotImplementedError()
+
+    def over(self):
+        return False
+
+    def make_move(self, move):
+        raise NotImplementedError()
+
+    def undo_move(self):
+        raise NotImplementedError()
+
+    def score(self):
+        return 0
+
+    def state(self):
+        raise NotImplementedError()
+
+    @classmethod
+    def load(cls, self):
+        raise NotImplementedError()
+
+    def copy(self):
+        return self.load(self.state())
