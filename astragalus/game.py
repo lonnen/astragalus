84ba8598d297517cf0c62e6fefe2edebefd55b12
lonnen <chris.lonnen@gmail.com>
2024-01-30 23:14:34 -0800

    let's get specific about imports

--- astragalus/game.py
+++ astragalus/game.py
@@ -2,7 +2,7 @@
 A library for the minigame Knucklebones from Cult of the Lamb
 """
 
-from .board import *  # noqa: F403,F401
+from .board import KnucklebonesBoard
 
 
 class KnucklebonesGame(object):
